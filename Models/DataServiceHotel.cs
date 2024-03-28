df1383daafa3ba6eab65561016af8510bd57cf96
Na-zi-mo <nazimad2004@gmail.com>
2024-03-28 14:34:48 -0400

    Initial Commit

---
+++ Models/DataServiceHotel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel24Eq5.Models
+{
+    public class DataServiceHotel
+    {
+        string connectionString;
+
+        #region ObservableCollection
+
+        private ObservableCollection<COMMODITE> commodites;
+        public ObservableCollection<COMMODITE> Commodites
+        {
+            get => commodites;
+            set
+            {
+                commodites = value;
+
+
+            }
+        }
+        private ObservableCollection<LOCALISATION> localisations;
+        public ObservableCollection<LOCALISATION> Localisations
+        {
+            get => localisations;
+            set
+            {
+                localisations = value;
+
+
+            }
+        }
+        private ObservableCollection<TYPECHAMBRE> typeChambres;
+        public ObservableCollection<TYPECHAMBRE> TypesChambres
+        {
+            get => typeChambres;
+            set
+            {
+                typeChambres = value;
+
+
+            }
+        }
+
+        private ObservableCollection<CHAMBRE> chambres;
+        public ObservableCollection<CHAMBRE> Chambres
+        {
+            get => chambres;
+            set
+            {
+                chambres = value;
+
+
+            }
+        }
+        private ObservableCollection<CLIENT> clients;
+        public ObservableCollection<CLIENT> Clients
+        {
+            get => clients;
+            set
+            {
+                clients = value;
+
+
+            }
+        }
+        private ObservableCollection<RESERVE> reserves;
+        public ObservableCollection<RESERVE> Reserves
+        {
+            get => reserves;
+            set
+            {
+                reserves = value;
+
+
+            }
+        }
+        private ObservableCollection<ARRIVE> arrives;
+        public ObservableCollection<ARRIVE> Arrives
+        {
+            get => arrives;
+            set
+            {
+                arrives = value;
+
+
+            }
+        }
+        private ObservableCollection<DEPART> departs;
+        public ObservableCollection<DEPART> Departs
+        {
+            get => departs;
+            set
+            {
+                departs = value;
+
+
+            }
+        }
+        private ObservableCollection<TYPETRX> typeTrxs;
+        public ObservableCollection<TYPETRX> TypeTrxs
+        {
+            get => typeTrxs;
+            set
+            {
+                typeTrxs = value;
+
+
+            }
+        }
+        private ObservableCollection<TRX> trxs;
+        public ObservableCollection<TRX> Trxs
+        {
+            get => trxs;
+            set
+            {
+                trxs = value;
+
+
+            }
+        }
+        #endregion
+        public DataServiceHotel()
+        {
+            connectionString = @"Data Source=LAPTOP-BA8LUHNR\SQLEXPRESS;Initial Catalog=bdhotel24;Integrated Security=True;Connect Timeout=30;Encrypt=False";
+
+            //LoadAll();
+
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using hotel24Eq5.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hotel24Eq5.ViewModels;
+
+namespace hotel24Eq5.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public RelayCommand CmdGotoAccueil { get; private set; }
+        public RelayCommand CmdGotoChambre { get; private set; }
+
+        private BaseViewModel currentViewModel;
+
+        public BaseViewModel CurrentViewModel
+        {
+            get => currentViewModel;
+            set
+            {
+                currentViewModel = value;
+                OnPropertyChanged();
+            }
+
+        }
+        public MainViewModel()
+        {
+
+            CurrentViewModel = new AccueilViewModel();
+            // le contentControl doit savoir afficher un AccueilViewModel
+
+            CmdGotoAccueil = new RelayCommand(GotoAccueil, null);
+            CmdGotoChambre = new RelayCommand(GotoChambre, null);
+        }
+
+        private void GotoAccueil(object obj)
+        {
+            CurrentViewModel = new AccueilViewModel();
+        }
+        private void GotoChambre(object obj)
+        {
+            CurrentViewModel = new ChambreViewModel();
+        }
+    }
+}
